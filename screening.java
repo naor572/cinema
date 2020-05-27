02e15e50518917e0eee878afddd767438cc80d03
naor <naor@gmail.com>
2020-05-27 21:18:04 +0300

    screening class

---
+++ screening.java
@@ -0,0 +1,25 @@
+package cinema;
+
+public class screening 
+
+{
+	private static int screenId;
+	private int movieId;
+	private int theaterId;
+	
+	public static int getScreenId() 
+	{
+		return screenId;
+	}
+
+	public int getMovieId() 
+	{
+		return movieId;
+	}
+
+	public int getTheaterId()
+	{
+		return theaterId;
+	}
+
+}
